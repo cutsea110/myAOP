b6cfd6207d3a771e87bf25b7fb665f3a3ed41ec8
cutsea110 <cutsea110@gmail.com>
2017-06-29 01:08:03 +0900

    cata for Exp, is this ok?

--- Exp.hs
+++ Exp.hs
@@ -2,6 +2,7 @@ module Exp where
 
 import Prelude hiding (curry)
 
+-- Exponentials
 data Exp a b = Exp (b -> a)
 
 curry :: ((c, b) -> a) -> c -> Exp a b
@@ -9,6 +10,12 @@ curry f c = Exp (\b -> f (c, b))
 
 apply :: (Exp a b, b) -> a
 apply (Exp g, x) = g x
+
+-- This is right?
+cata :: (a -> (c, b)) -> (Exp a b, b) -> (c, b)
+cata phi = mu
+  where
+    mu (Exp g, b) = phi (g b)
 
 ana :: ((c, b) -> a) -> (c, b) -> (Exp a b, b)
 ana psi = nu
