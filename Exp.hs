267dd9dd632470ebacb4a7790d9538fb07bdf0fc
cutsea110 <cutsea110@gmail.com>
2017-06-28 11:00:43 +0900

    add Exp a

---
+++ Exp.hs
@@ -0,0 +1,16 @@
+module Exp where
+
+import Prelude hiding (curry)
+
+data Exp a b = Exp (b -> a)
+
+curry :: ((c, b) -> a) -> c -> Exp a b
+curry f = \c -> Exp (\b -> f (c, b))
+
+apply :: (Exp a b, b) -> a
+apply (Exp g, x) = g x
+
+ana :: ((c, b) -> a) -> (c, b) -> (Exp a b, b)
+ana psi = nu
+  where
+    nu (c, b) = (curry psi c, b)
