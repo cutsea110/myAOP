a656a1f39734bbb9c5914eee8c30be368a7be23c
cutsea110 <cutsea110@gmail.com>
2017-06-28 09:27:51 +0900

    add Either

--- Triv.hs
+++ Triv.hs
@@ -1,19 +1,35 @@
 module Triv where
 
-import Prelude hiding (maybe)
+import Prelude hiding (maybe, either)
 
 -- Maybe
 data Option a = None | Some a deriving Show
 
 maybe :: (x, a -> x) -> Option a -> x
-maybe (c, f) = u
+maybe (c, f) = mu
   where
-    u None = c
-    u (Some a) = f a
+    mu None = c
+    mu (Some a) = f a
 
 unmaybe :: (x -> Maybe a) -> x -> Option a
-unmaybe phi = u
+unmaybe psi = nu
   where
-    u x = case phi x of
+    nu x = case psi x of
       Nothing -> None
       Just a -> Some a
+
+-- Either
+data Gender a b = Male a | Female b deriving Show
+
+either :: (a -> x, b -> x) -> Gender a b -> x
+either (f, g) = mu
+  where
+    mu (Male a) = f a
+    mu (Female b) = g b
+
+uneither :: (x -> Either a b) -> x -> Gender a b
+uneither psi = nu
+  where
+    nu x = case psi x of
+      Left a -> Male a
+      Right b -> Female b
